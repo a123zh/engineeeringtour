8c8c7f8e3a82f82ae3095a8cf5522f9e238cdd0a
a123zh <e0675590@u.nus.edu>
2021-12-10 11:06:54 +0800

    first commit
    this is the first upload of all the files

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.167109862246285,
+        "pitch": -0.3657380577668441,
+        "fov": 1.513843149153066
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8703570280436281,
+          "pitch": -0.15439172465032414,
+          "rotation": 3.9269908169872414,
+          "target": "2-along-engineering-drive-1"
+        },
+        {
+          "yaw": 0.13682818441332323,
+          "pitch": -0.02939101981474579,
+          "rotation": 0,
+          "target": "1-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.167109862246285,
+          "pitch": -0.3657380577668441,
+          "title": "engineering adutorium",
+          "text": "this is the auditorium of engineering ,there're many seats for students study at&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.8044633361516977,
+        "pitch": -0.191151950813353,
+        "fov": 1.513843149153066
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1348237646549517,
+          "pitch": 0.016016294994454228,
+          "rotation": 3.9269908169872414,
+          "target": "0-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8094627347173375,
+          "pitch": -0.0032324875673914732,
+          "title": "engineering auditorium atrium",
+          "text": "the coffee is great!"
+        }
+      ]
+    },
+    {
+      "id": "2-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.609936540765954,
+        "pitch": 0.45772271688168686,
+        "fov": 1.513843149153066
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.046660869411967454,
+          "pitch": -0.3585137637508211,
+          "rotation": 7.0685834705770345,
+          "target": "0-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7914429683076349,
+          "pitch": 0.07532495318996979,
+          "title": "Faculty of Engineering",
+          "text": "welcome to the faculty of engineering"
+        }
+      ]
+    }
+  ],
+  "name": "faculty of engineering",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
